f6ccd49850fafda29cc877210b3caa83fd06896d
Marvelousmolokwu <qmolokwu@gmail.com>
2023-02-07 10:22:53 +0100

    added project 10

---
+++ 10-the checkbox project/script.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.inbox input[type ="checkbox"]');
+let lastChecked;
+function handlecheck(e){
+      let inBetween = false;
+    if(e.shiftKey && this.checked){
+      checkboxes.forEach(checkbox=>{
+        console.log('me')
+        if(checkbox === this || checkbox === lastChecked){
+            inBetween = !inBetween
+            console.log("me!")
+        }
+        if(inBetween){
+            checkbox.checked = true;
+        }
+      })
+
+    }
+lastChecked = this;
+
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener("click", handlecheck));
